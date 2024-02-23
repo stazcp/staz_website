53b988e65cb5f75c99d15cb63aba2938d97d6009
stazcp <staz.christo@gmail.com>
2024-02-23 13:57:47 -0800

    Add aiResponsePendingAtom to useChat hook

--- src/hooks/use-chat.tsx
+++ src/hooks/use-chat.tsx
@@ -1,12 +1,13 @@
 import { useQuery, useMutation } from '@tanstack/react-query'
 import { keepServerAwake, startConversation, sendMessageToServer } from '../api'
 import { useAtom } from 'jotai'
-import { aiResponseAtom } from '@store/chat-state'
+import { aiResponseAtom, aiResponsePendingAtom } from '@store/chat-state'
 
 export const SEND_MESSAGE = ['sendMessage']
 
 const useChat = () => {
   const [aiResponse, setAiResponse] = useAtom(aiResponseAtom)
+  const [aiResponsePending, setAiResponsePendingAtom] = useAtom(aiResponsePendingAtom)
 
   const {
     isPending: serverConnectionPending,
@@ -23,14 +24,12 @@ const useChat = () => {
     refetchInterval: 49000,
   })
 
-  const {
-    mutate: sendMessage,
-    error: aiResponseError,
-    isPending: aiResponsePending,
-  } = useMutation<string, string>({
+  const { mutate: sendMessage, error: aiResponseError } = useMutation<string, string>({
     mutationKey: SEND_MESSAGE,
     mutationFn: (userInput) => sendMessageToServer(threadId!, userInput!),
     onSuccess: (data) => setAiResponse(data),
+    onSettled: () => setAiResponsePendingAtom(false),
+    onMutate: () => setAiResponsePendingAtom(true),
   })
 
   return {
--- src/store/chat-state.ts
+++ src/store/chat-state.ts
@@ -2,3 +2,4 @@ import { INTRO_TEXT } from '@constants'
 import { atom } from 'jotai'
 
 export const aiResponseAtom = atom(INTRO_TEXT)
+export const aiResponsePendingAtom = atom(false)
